8f26ce82600cc40e0aae12ea8e1b7fa4fab1069b
Addison Broschell <abrosche@uoguelph.ca>
2025-09-29 12:07:25 -0400

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -8,24 +8,27 @@
 
 #Lab 3 Part 1: Fibonacci Sequence
 # Setting up the loop
-while True:
-    term_input = input("Enter the number of terms: ")
+while True: # starts the infintie loop
+    term_input = input("Enter the number of terms: ") # getting the user input for the amount of terms
     try:
         terms = int(term_input)
-        if terms <= 0:
+        if terms <= 0: # checks if it is a non-positive integer
             print("Please enter a positive integer.")
         else:
             break
-    except ValueError:
+    except ValueError: # if the user input is not valid prints the error
         print("Error! Invalid input, please enter a valid input.")
+
+# print the user input
+print("User Input: ", terms)
 
 #Print the Fibonacci sequence
 #Fibonacci Sequence: 0 1 1 2 3 5 8 13 21 34 55 89 144
 a = 0
 b = 1
-for i in range(terms):
+for i in range(terms): 
     print(a, end=' ')
-    a, b = b, a + b
+    a, b = b, a + b # the new value of a becomes b's old value, the new value of b is the old a and b added together
           
 
 
